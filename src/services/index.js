8524ecee4efe13bb22bb51f168225f7826dd7890
Gabriel West <gabe_west1@yahoo.com>
2017-07-22 01:14:56 -0500

    Added service function to query wikipedia for articles

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,25 @@
 /**
  * Put 3rd Pary APIs here
  */
+import $ from "jquery"
+
+export function queryWikiArticles(query) {
+    const URL = `https://en.wikipedia.org/w/api.php?action=query&srsearch=${query}&list=search&format=json`
+    const options = {
+        url: URL,
+        dataType: "jsonp",
+        jsonp: "callback",
+        headers: {"Access-Control-Allow-Origin": "*"},
+    }
+
+    return new Promise((resolve) => {
+        $.ajax(options).then(response => {
+            console.log("HELLOO")
+            console.log(response)
+            let articles = response.query.search
+            console.log("HELLOOfdsfdsfdsfds")
+            console.log(articles)
+            resolve(articles)
+        })
+    })
+}
