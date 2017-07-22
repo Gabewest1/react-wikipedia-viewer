271d26690fecfae4a68cba64fedc813bcaa3bdd8
Gabriel West <gabe_west1@yahoo.com>
2017-07-22 01:12:47 -0500

    Added reducer for redux-form

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux"
 import { default as wikiArticles } from "redux/wikiArticles"
+import { reducer as formReducer } from "redux-form"
 
-export default combineReducers({wikiArticles})
+export default combineReducers({wikiArticles, form: formReducer})
