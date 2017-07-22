cb8e48c49599e15e62e58588a7da37ea1b12864b
Gabriel West <gabe_west1@yahoo.com>
2017-07-21 21:45:27 -0500

    Minor fix

---
+++ src/redux/wikiArticles/index.js
@@ -0,0 +1,7 @@
+import reducer from "./reducers"
+
+// export { default as wikiArticleSelectors } from "./selectors"
+export { actions } from "./reducers"
+export { default as sagas } from "./sagas"
+
+export default reducer
