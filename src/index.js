3b8b8c960b6fc02abca13ec156fe136f6b3803fe
Gabriel West <gabe_west1@yahoo.com>
2017-07-23 01:22:55 -0500

    Added import for index.css and replace single quotes with double quotes

--- src/index.js
+++ src/index.js
@@ -1,44 +1,45 @@
-import 'babel-polyfill';
+import "babel-polyfill";
+import "style-loader!css-loader!index.css"
 
-import { AppContainer } from 'react-hot-loader';
-import React from 'react';
-import { render } from 'react-dom';
+import { AppContainer } from "react-hot-loader";
+import React from "react";
+import { render } from "react-dom";
 
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 
-import store from './configureStore.js';
-import App from './components/App';
+import store from "./configureStore.js";
+import App from "./components/App";
 
 
 render(
-  <AppContainer>
-    <Provider store={ store }>
-      <App />
-    </Provider>
-  </AppContainer>,
-  document.getElementById('root-entry')
+    <AppContainer>
+        <Provider store={ store }>
+            <App />
+        </Provider>
+    </AppContainer>,
+    document.getElementById("root-entry")
 );
 
 // Handle hot reloading requests from Webpack
 if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    //If we receive a HMR request for our App container, then
-    //reload it using require (we can't do this dynamically with import)
-    const NextApp = require('./components/App').default;
+    module.hot.accept("./components/App", () => {
+        //If we receive a HMR request for our App container, then
+        //reload it using require (we can"t do this dynamically with import)
+        const NextApp = require("./components/App").default;
 
-    // And render it into the root element again
-    render(
-      <AppContainer>
-        <Provider store={ store }>
-          <NextApp />
-        </Provider>
-      </AppContainer>,
-      document.getElementById('root-entry')
-    );
-  });
+        // And render it into the root element again
+        render(
+            <AppContainer>
+                <Provider store={ store }>
+                    <NextApp />
+                </Provider>
+            </AppContainer>,
+            document.getElementById("root-entry")
+        );
+    });
 
-  module.hot.accept('./reducers', () => {
-    const newReducer = require('./reducers').default;
-    store.replaceReducer(newReducer);
-  });
+    module.hot.accept("./reducers", () => {
+        const newReducer = require("./reducers").default;
+        store.replaceReducer(newReducer);
+    });
 }
