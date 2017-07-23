d707873d9a871b72076647bcfd98e8ca43eaafc6
Gabriel West <gabe_west1@yahoo.com>
2017-07-23 01:23:23 -0500

    Added wikiGlobe reducer to root reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,10 @@
 import { combineReducers } from "redux"
 import { default as wikiArticles } from "redux/wikiArticles"
 import { reducer as formReducer } from "redux-form"
+import { default as wikiGlobe } from "redux/wikiGlobe"
 
-export default combineReducers({wikiArticles, form: formReducer})
+export default combineReducers({
+    wikiArticles,
+    form: formReducer,
+    wikiGlobe
+})
