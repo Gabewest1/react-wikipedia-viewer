6d4e3467fd7c0d7271b5deda3af5e8f3ccdd8a85
Gabriel West <gabe_west1@yahoo.com>
2017-07-25 19:03:26 -0500

    Updated url to query wiki articles. Updated handling of the response from querying the articles

--- src/services/index.js
+++ src/services/index.js
@@ -4,7 +4,7 @@
 import $ from "jquery"
 
 export function queryWikiArticles(query) {
-    const URL = `https://en.wikipedia.org/w/api.php?action=query&srsearch=${query}&list=search&format=json`
+    const URL = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&format=json`
     const options = {
         url: URL,
         dataType: "jsonp",
@@ -14,12 +14,8 @@ export function queryWikiArticles(query) {
 
     return new Promise((resolve) => {
         $.ajax(options).then(response => {
-            console.log("HELLOO")
             console.log(response)
-            let articles = response.query.search
-            console.log("HELLOOfdsfdsfdsfds")
-            console.log(articles)
-            resolve(articles)
+            resolve(response)
         })
     })
 }
