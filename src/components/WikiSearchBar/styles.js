436af868f5ddd2134ad5c21ddd181e00a02e55f8
Gabriel West <gabe_west1@yahoo.com>
2017-07-25 19:49:48 -0500

    Changed styles to animate its width from 0% to x%

--- src/components/WikiSearchBar/styles.js
+++ src/components/WikiSearchBar/styles.js
@@ -1,13 +1,17 @@
 import styled from "styled-components"
 import { ReactHeight } from "react-height"
 
+//The animation timing is coupled with the animation timing in the wikiGlobe component
 export const Wrapper = styled(ReactHeight)`
     display: flex;
     z-index: ${({ isExpanded }) => isExpanded ? "1" : "auto"};
+    width: ${({ isExpanded }) => isExpanded ? "100%" : "0%"};
+    transition: width 1s ease-in-out 100ms;
 `
 export const Input = styled.input`
     padding: 10px 15px;
     outline: none;
+    width: 100%
 `
 export const Button = styled.button`
     background: url(assets/images/icons/search.svg);
