ef2d0b1ed667b6d5874b2a9008a079e2b8b1cbba
Gabriel West <gabe_west1@yahoo.com>
2017-07-28 15:06:23 -0500

    Imported sagas from `wikiIframe`

--- src/sagas.js
+++ src/sagas.js
@@ -1,8 +1,10 @@
-import { all } from "redux-saga/effects"
-import { sagas as wikiArticleSagas } from "redux/wikiArticles"
+import { fork } from "redux-saga/effects"
+import { sagas as wikiArticleSagas } from "Redux/wikiArticles"
+import { sagas as wikiIframeSagas } from "Redux/wikiIframe"
 
 export default function* rootSaga() {
-    yield all([
-        wikiArticleSagas()
-    ])
+    yield [
+        fork(wikiArticleSagas),
+        fork(wikiIframeSagas)
+    ]
 }
