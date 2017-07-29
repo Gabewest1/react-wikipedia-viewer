92744468079f3e888ce5c391196d7aecbcfb2cb5
Gabriel West <gabe_west1@yahoo.com>
2017-07-28 20:48:17 -0500

    Added animation for when articles enter the screen

--- src/components/WikiArticles/styles.js
+++ src/components/WikiArticles/styles.js
@@ -1,22 +1,35 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import colors from "Lib/colors"
 
 const itemMargin = 15
+
+const WikiArticleAnimation = keyframes`
+    from { opacity: 0 }
+    to { opacity: 1 }
+`
 
 export const WikiArticles = styled.ul`
     background: ${colors.secondary};
     box-sizing: border-box;
     padding: 0;
     padding-top: 10px;
-    margin: ${({ margin }) => margin};
     overflow-y: scroll;
+    margin: ${({ margin }) => margin};
     height: ${({ height }) => height};
 `
 
 export const WikiArticle = styled.li`
+    cursor: pointer;
     margin-left: 10px;
     border-left: solid 7px black;
     padding-left: 10px;
+    animation: ${WikiArticleAnimation} 1s ease-in-out;
+
+    &:hover {
+        h1 {
+            color: ${colors.primary};
+        }
+    }
 `
 
 export const WikiArticleTitle = styled.h1`
