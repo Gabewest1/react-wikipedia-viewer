34bf4394dd06b8ed0f7548b402e73e33148db527
Gabriel West <gabe_west1@yahoo.com>
2017-07-28 20:20:43 -0500

    Added wikiIframe state, actions and selectors. Created handlers for closing the iFrame and when its animations finish

--- src/components/WikiIframe/index.js
+++ src/components/WikiIframe/index.js
@@ -3,26 +3,45 @@ import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
 
 import { actions as wikiArticleActions, selectors as wikiArticleSelectors } from "Redux/wikiArticles"
+import { actions as wikiIframeActions, selectors as wikiIframeSelectors } from "Redux/wikiIframe"
+
 import { WikiIframeView } from "./views"
 
 class WikiIframe extends React.Component {
+    onClick = () => {
+        this.props.fadeOutIframe()
+    }
+    onTransitionEnd = (e) => {
+        let transitionName = e.propertyName
+
+        if (transitionName === "width") {
+            this.props.expandIframeFinished()
+        }
+    }
     render() {
         return (
-            <WikiIframeView { ...this.props } />
+            <WikiIframeView
+                { ...this.props }
+                onClick= { this.onClick }
+                onTransitionEnd= { this.onTransitionEnd } />
         )
     }
 }
 
 function mapStateToProps(state) {
-    let { wikiArticles } = state
+    let { wikiArticles, wikiIframe } = state
 
     return {
-        src: wikiArticleSelectors.selectCurrentArticleUrl(wikiArticles)
+        src: wikiArticleSelectors.selectCurrentArticleUrl(wikiArticles),
+        isFadedIn: wikiIframeSelectors.selectFadeInStatus(wikiIframe),
+        isExpanded: wikiIframeSelectors.selectExpandedStatus(wikiIframe),
+        fadeIn: wikiIframeSelectors.selectFadeIn(wikiIframe),
+        fadeOut: wikiIframeSelectors.selectFadeOut(wikiIframe)
     }
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(wikiArticleActions, dispatch)
+    return bindActionCreators({...wikiArticleActions, ...wikiIframeActions}, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(WikiIframe)
