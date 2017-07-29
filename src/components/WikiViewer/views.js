02a38ea4d4434e8b657519825587dcb54e5dbaec
Gabriel West <gabe_west1@yahoo.com>
2017-07-28 20:33:38 -0500

    Removed unused code

--- src/components/WikiViewer/views.js
+++ src/components/WikiViewer/views.js
@@ -1,14 +1,2 @@
 import React from "react" 
-import { ExpandGlobeButton, Modal, TintedBackground } from "./styles"
 
-export const ExpandGlobeButtonView = (props) => {
-    let { isExpanded, expandGlobe, collapseGlobe, children } = props
-
-    let onClick = isExpanded ? collapseGlobe : expandGlobe
-    let text = children ? children :
-               isExpanded ? "Close Globe" : "Open the Earth"
-
-    return (
-        <ExpandGlobeButton onClick={ () => onClick() }>{ text }</ExpandGlobeButton>
-    )
-}
